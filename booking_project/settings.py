0a975c59cd83e5dc34e7af288d15ce73a9cbec88
isaiaha09 <isaiah.aris@gmail.com>
2025-10-14 16:12:11 -0700

    pushing databases pt2

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -92,12 +92,23 @@ WSGI_APPLICATION = 'booking_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
-}
-
+# Use SQLite locally if running on your PC
+# Detect by checking if "RUNNING_LOCALLY" env variable is set
+if os.environ.get("RUNNING_LOCALLY") == "1":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Use DATABASE_URL from .env (for Render)
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=os.environ.get('DATABASE_URL'),
+            conn_max_age=600,
+        )
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

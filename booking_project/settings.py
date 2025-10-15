5ab91be25c5263902fd32cf4bcf3a87a64b9f19f
isaiaha09 <isaiah.aris@gmail.com>
2025-10-14 19:35:48 -0700

    settings back to normal (postsql and sqllite3 separated)

--- booking_project/settings.py
+++ booking_project/settings.py
@@ -95,7 +95,17 @@ WSGI_APPLICATION = 'booking_project.wsgi.application'
 # Use SQLite locally if running on your PC
 # Detect by checking if "RUNNING_LOCALLY" env variable is set
 
-DATABASES = {
+
+if os.environ.get("RUNNING_LOCALLY") == "1":
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    # Use Render's PostgreSQL
+    DATABASES = {
         'default': dj_database_url.config(
             default=os.environ.get('DATABASE_URL'),
             conn_max_age=600,
